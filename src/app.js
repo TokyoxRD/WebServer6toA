de93bbdfac2998da3c30d9fc7fb31da58bfa5c5e
Hansel Polanco <Hanselsamp73@gmail.com>
2026-03-10 07:47:21 -0400

    Cambios para que funcione el render

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
-import express from 'express';
+  import express from 'express';
+  import path from 'path';
+  import { fileURLToPath } from 'url';
 
-const port = process.env.PORT || 3000;
-const app = express();
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const port = process.env.PORT || 3000;
+  
+  const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+  console.log("_dirname: $", {__dirname});
 
-app.listen(port, () => {
-  console.log(`Server is running on https://localhost:${port}`);
-});
+
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+
+  app.get('/', (req, res) => {
+    res.render("index");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on https://localhost:${port}`);
+  });
